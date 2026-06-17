4268b5f06cb35396dc8fec1f14838152de350adb
witheredwav <steam.akk1337@bk.ru>
2026-06-17 22:37:07 +0500

    Update functions.py

--- app/db/functions.py
+++ app/db/functions.py
@@ -1,29 +1,17 @@
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload
 
 from app.db.models import User
 from app.db.session import async_session_factory
 
 
-async def get_or_create_user(user_id: int, username: str | None) -> User:
+async def get_user(user_id: int) -> User | None:
     async with async_session_factory() as session:
 
         result = await session.execute(
-            select(User).where(User.telegram_id == user_id)
-        )
-        user = result.scalar_one_or_none()
-
-        if user:
-            return user
-
-        # 🔥 СОЗДАНИЕ ПОЛЬЗОВАТЕЛЯ
-        user = User(
-            telegram_id=user_id,   # ❗ ВОТ ЭТО ОБЯЗАТЕЛЬНО
-            username=username,
-            is_active=True
+            select(User)
+            .options(selectinload(User.roles))  # 🔥 ВАЖНО
+            .where(User.telegram_id == user_id)
         )
 
-        session.add(user)
-        await session.commit()
-        await session.refresh(user)
-
-        return user
+        return result.scalar_one_or_none()
