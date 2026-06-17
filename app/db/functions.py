a8b79b7db0295ca23db6f3bd8277ba536ea4b87e
witheredwav <steam.akk1337@bk.ru>
2026-06-17 22:30:05 +0500

    Update functions.py

--- app/db/functions.py
+++ app/db/functions.py
@@ -1,12 +1,27 @@
 from sqlalchemy import select
-
 from app.db.models import User
 from app.db.session import async_session_factory
 
 
-async def get_user(user_id: int) -> User | None:
+async def get_or_create_user(user_id: int, username: str | None = None) -> User:
     async with async_session_factory() as session:
+
         result = await session.execute(
             select(User).where(User.id == user_id)
         )
-        return result.scalar_one_or_none()
+        user = result.scalar_one_or_none()
+
+        if user:
+            return user
+
+        # 🔥 если нет — создаём
+        user = User(
+            id=user_id,
+            username=username,
+        )
+
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+
+        return user
