a39eb12fbacbbe64d35c08b3bb7a6ac2e773eaf4
witheredwav <steam.akk1337@bk.ru>
2026-06-17 23:47:24 +0500

    Update user_context.py

--- app/bot/middlewares/user_context.py
+++ app/bot/middlewares/user_context.py
@@ -1,33 +1,31 @@
-from typing import Callable, Dict, Any, Awaitable
-
 from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject
+from typing import Callable, Awaitable, Dict, Any
 
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
-from app.db.functions import get_user
+from app.db.models import User
+from app.services.users import get_user
 
 
 class UserContextMiddleware(BaseMiddleware):
-
     def __init__(self, sessionmaker: async_sessionmaker):
         self.sessionmaker = sessionmaker
 
     async def __call__(
         self,
-        handler: Callable,
-        event: TelegramObject,
+        handler: Callable[[Any, Dict[str, Any]], Awaitable[Any]],
+        event: Any,
         data: Dict[str, Any],
-    ) -> Any:
-
+    ):
         tg_user = getattr(event, "from_user", None)
 
+        if not tg_user:
+            return await handler(event, data)
+
         async with self.sessionmaker() as session:
+            db_user: User | None = await get_user(session, tg_user.id)
 
-            data["session"] = session
-
-            if tg_user:
-                db_user = await get_user(session, tg_user.id)
-                data["db_user"] = db_user
+            data["db_user"] = db_user
+            data["session"] = session  # 👈 ВАЖНО
 
             return await handler(event, data)
