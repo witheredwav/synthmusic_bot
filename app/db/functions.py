289fa12deb5cae485962e0d45c32ac0d4309b258
witheredwav <steam.akk1337@bk.ru>
2026-06-17 22:26:06 +0500

    Update functions.py

--- app/db/functions.py
+++ app/db/functions.py
@@ -1,10 +1,11 @@
 from sqlalchemy import select
+
 from app.db.models import User
-from app.db.session import async_session  # ⚠️ если у тебя другое имя — скажешь
+from app.db.session import async_session_factory
 
 
 async def get_user(user_id: int) -> User | None:
-    async with async_session() as session:
+    async with async_session_factory() as session:
         result = await session.execute(
             select(User).where(User.id == user_id)
         )
